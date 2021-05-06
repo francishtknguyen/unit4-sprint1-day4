c764d7bbbf675ab7a86a55db329507cd6a9ab215
Francis Nguyen <francishtknguyen@gmail.com>
2021-05-06 12:57:17 -0500

    added client

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 require("dotenv").config();
+const path = require("path");
 const express = require("express");
 
 const server = express();
 
 const PORT = process.env.PORT || 5000;
+
+process.env.API_KEY || "gaga";
+
+server.use(express.static(path.join(__dirname, "client/build")));
 
 server.use(express.json());
 
@@ -11,6 +16,10 @@ server.get("/api/*", (req, res) => {
   res.json({
     cohort: "Web 41",
   });
+});
+
+server.use("*", (_, res) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
 });
 
 server.listen(PORT, () => {
